e2deb82e5f84a17215f9ff1e051779221ddcf90a
Victor A. Martínez Santiago <victor@plazsa.com>
2017-04-01 17:00:13 -0400

    added makeTables function

--- db/index.js
+++ db/index.js
@@ -1,12 +1,13 @@
 const r = require('rethinkdb')
 
 
+
 /**
  * connects to a rethinkdb database
  * 
- * @export connect
- * @param {Object} options connection options such as port, host, and db
- * @returns {Connection|Error} 
+ * @export
+ * @param {any} { port = 28015, host = 'localhost', db } 
+ * @returns {any} { conn = rethinkdb.Connection|undefined, err = Error|undefined }
  */
 export async function connect({ port = 28015, host = 'localhost', db }) {
     try {
@@ -16,6 +17,42 @@ export async function connect({ port = 28015, host = 'localhost', db }) {
             db: db,
         })
         return { conn }
+    } catch (err) {
+        return { err }
+    }
+}
+
+
+/**
+ * creates the tables it was sent out to make on the database you choose
+ * 
+ * @export
+ * @param {any} { conn, database = "", tables = string[] } 
+ * @returns {any} { results = rethinkdb.CreateResult[]|undefined, err = Error|undefined }
+ */
+export async function makeTables({ conn, database = "test", tables = [] }) {
+    try {
+        let databases = await r.dbList().run(conn)
+
+        if (databases.indexOf(database) === -1) {
+            throw new Error(`Database ${database} was not found.`)
+        }
+
+        let db = r.db(database),
+            list = await db.tableList().run(conn)
+
+        // only create tables that don't exist
+        tables = tables.map(table => {
+            if (list.indexOf(table) !== -1) {
+                return table
+            }
+        })
+
+        let results = tables.map(table => {
+            await db.tableCreate(table).run(conn)
+        })
+
+        return { results }
     } catch (err) {
         return { err }
     }
