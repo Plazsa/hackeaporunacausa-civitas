e1c51c4bef0d2c21bee723549694a1eb5d7d5cf2
Victor A. Martínez Santiago <victor@plazsa.com>
2017-04-01 20:10:49 -0400

    moved stuff

--- db/index.js
+++ db/index.js
@@ -1,6 +1,5 @@
+// Package db
 const r = require('rethinkdb')
-
-
 
 /**
  * connects to a rethinkdb database
@@ -15,36 +14,4 @@ module.exports.connect = async function connect({ port = 28015, host = 'localhos
         host: host,
         db: db,
     })
-}
-
-
-/**
- * creates the tables it was sent out to make on the database you choose
- * 
- * @export
- * @param {any} { conn, database = "", tables = string[] } 
- * @returns {CreateResult} results
- */
-module.exports.makeTables = async function makeTables({ conn, database = "test", tables = [] }) {
-
-    let databases = await r.dbList().run(conn)
-
-    if (databases.indexOf(database) === -1) {
-        throw new Error(`Database ${database} was not found.`)
-    }
-
-    let db = r.db(database),
-        list = await db.tableList().run(conn)
-
-    // only create tables that don't exist
-    tables = tables.filter(table => {
-        if (list.indexOf(table) === -1) {
-            return table
-        }
-    })
-
-    // create tables and map the resutls of their creation
-    return await Promise.all(tables.map(async table => {
-        return await db.tableCreate(table).run(conn)
-    }))
 }
---
+++ main.js
@@ -0,0 +1,27 @@
+const Koa = require('koa'),
+    logger = require('koa-logger'),
+    flags = require('flags'),
+    r = require('rethinkdb'),
+    routers = require('./routers'),
+    db = require('./db')
+
+async function main() {
+    flags.defineInteger('port', 5000, 'Server port')
+    flags.defineString('rethinkdb-database', 'agora', 'RethinkDB Database to use')
+    flags.defineString('rethinkdb-host', 'localhost', 'RethinkDB host to use for connection')
+    flags.defineInteger('rethinkdb-port', 28015, 'RethinkDB port number to use for connection')
+    
+    flags.parse()
+
+    let tables = ['volunteers', 'events', 'communities', 'comments', 'announcements'],
+        options = { db: flags.get('rethinkdb-database'), port: flags.get('rethinkdb-port'), host: flags.get('rethinkdb-host') },
+        app = new Koa()
+
+    app.use(logger())
+
+    app.listen(flags.get('port'), () => {
+        console.info('listening on port', flags.get('port'))
+    })
+}
+
+module.exports.main = main
